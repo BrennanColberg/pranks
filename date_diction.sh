522d62140b78cb17a7907dbe76a6f84eee5ea185
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-02 00:04:55 -0700

    Reformatting
    - renamed .command files to .sh
    - moved technical detail comments to the top of each file
    - moved qualitative description comments to whitespace-separated paragraph between technical details and code

--- date_diction.sh
+++ date_diction.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# works on macOS, untested on Windows/Linux
+
 # text-to-speech's then says the date and time through the current audio channel every single minute, on the minute, overriding any playing music/videos/etc
 # found online, not my creation
-# works on macOS, untested on Windows/Linux
+
 echo '* * * * * say "$(date)"' | crontab
